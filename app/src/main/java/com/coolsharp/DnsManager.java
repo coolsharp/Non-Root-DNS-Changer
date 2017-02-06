ce3a8a6ace75a43a539d91fe11db61240f3bec77
coolsharp <coolsharp@gmail.com>
2017-02-06 18:24:34 +0900

    소스 리펙토링

--- app/src/main/java/com/coolsharp/DnsManager.java
+++ app/src/main/java/com/coolsharp/DnsManager.java
@@ -7,6 +7,7 @@ import android.net.NetworkInfo;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.text.TextUtils;
 import android.text.format.Formatter;
 
@@ -22,8 +23,21 @@ import java.util.ArrayList;
  */
 
 public class DnsManager {
+    // [final/static_property]====================[START]===================[final/static_property]
+
     /** DNS2 값 */
     final static public String DNS2 = "10.102.4.1"; // Intercept everything
+
+    // [final/static_property]=====================[END]====================[final/static_property]
+    // [private/protected/public_property]========[START]=======[private/protected/public_property]
+    // [private/protected/public_property]=========[END]========[private/protected/public_property]
+    // [interface/enum/inner_class]===============[START]==============[interface/enum/inner_class]
+    // [interface/enum/inner_class]================[END]===============[interface/enum/inner_class]
+    // [inherited/listener_method]================[START]===============[inherited/listener_method]
+    // [inherited/listener_method]=================[END]================[inherited/listener_method]
+    // [life_cycle_method]========================[START]=======================[life_cycle_method]
+    // [life_cycle_method]=========================[END]========================[life_cycle_method]
+    // [private_method]===========================[START]==========================[private_method]
 
     private static Object newInstance(String className) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException {
         return newInstance(className, new Class<?>[0], new Object[0]);
@@ -95,6 +109,74 @@ public class DnsManager {
         return ssid;
     }
 
+    private static void setIpAssignment(String assign, WifiConfiguration wifiConf)
+            throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
+        setEnumField(wifiConf, assign, "ipAssignment");
+    }
+
+    private static void setIpAddress(InetAddress addr, int prefixLength, WifiConfiguration wifiConf)
+            throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException,
+            NoSuchMethodException, ClassNotFoundException, InstantiationException, InvocationTargetException {
+        Object linkProperties = getField(wifiConf, "linkProperties");
+        if (linkProperties == null) return;
+        Class laClass = Class.forName("android.net.LinkAddress");
+        Constructor laConstructor = laClass.getConstructor(new Class[]{InetAddress.class, int.class});
+        Object linkAddress = laConstructor.newInstance(addr, prefixLength);
+
+        ArrayList mLinkAddresses = (ArrayList) getDeclaredField(linkProperties, "mLinkAddresses");
+        mLinkAddresses.clear();
+        mLinkAddresses.add(linkAddress);
+    }
+
+    private static void setGateway(InetAddress gateway, WifiConfiguration wifiConf)
+            throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException,
+            ClassNotFoundException, NoSuchMethodException, InstantiationException, InvocationTargetException {
+        Object linkProperties = getField(wifiConf, "linkProperties");
+        if (linkProperties == null) return;
+        Class routeInfoClass = Class.forName("android.net.RouteInfo");
+        Constructor routeInfoConstructor = routeInfoClass.getConstructor(new Class[]{InetAddress.class});
+        Object routeInfo = routeInfoConstructor.newInstance(gateway);
+
+        ArrayList mRoutes = (ArrayList) getDeclaredField(linkProperties, "mRoutes");
+        mRoutes.clear();
+        mRoutes.add(routeInfo);
+    }
+
+    private static Object getField(Object obj, String name)
+            throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        Field f = obj.getClass().getField(name);
+        Object out = f.get(obj);
+        return out;
+    }
+
+    private static Object getDeclaredField(Object obj, String name)
+            throws SecurityException, NoSuchFieldException,
+            IllegalArgumentException, IllegalAccessException {
+        Field f = obj.getClass().getDeclaredField(name);
+        f.setAccessible(true);
+        Object out = f.get(obj);
+        return out;
+    }
+
+    private static void setEnumField(Object obj, String value, String name)
+            throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        Field f = obj.getClass().getField(name);
+        f.set(obj, Enum.valueOf((Class<Enum>) f.getType(), value));
+    }
+
+    private static void setDNS(InetAddress dns, WifiConfiguration wifiConf)
+            throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
+        Object linkProperties = getField(wifiConf, "linkProperties");
+        if (linkProperties == null) return;
+
+        ArrayList<InetAddress> mDnses = (ArrayList<InetAddress>) getDeclaredField(linkProperties, "mDnses");
+        mDnses.clear(); //or add a new dns address , here I just want to replace DNS1
+        mDnses.add(dns);
+    }
+
+    // [private_method]============================[END]===========================[private_method]
+    // [public_method]============================[START]===========================[public_method]
+
     public static void setDns(Context context, String dns) {
         WifiManager manager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         final DhcpInfo dhcp = manager.getDhcpInfo();
@@ -104,90 +186,28 @@ public class DnsManager {
 
         if (wifiConf != null) {
             try {
-                setStaticIpConfiguration(manager, wifiConf,
-                        InetAddress.getByName(ip), 24,
-                        InetAddress.getByName(gateWay),
-                        new InetAddress[]{InetAddress.getByName(dns), InetAddress.getByName(DNS2)});
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT){
+                    setIpAssignment("STATIC", wifiConf); //or "DHCP" for dynamic setting
+                    setIpAddress(InetAddress.getByName(ip), 24, wifiConf);
+                    setGateway(InetAddress.getByName(gateWay), wifiConf);
+                    setDNS(InetAddress.getByName(dns), wifiConf);
+                    manager.updateNetwork(wifiConf); //apply the setting
+                    manager.saveConfiguration(); //Save it
+                }
+                else {
+                    setStaticIpConfiguration(manager, wifiConf,
+                            InetAddress.getByName(ip), 24,
+                            InetAddress.getByName(gateWay),
+                            new InetAddress[]{InetAddress.getByName(dns), InetAddress.getByName(DNS2)});
+                }
             } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            try{
-                setIpAssignment("STATIC", wifiConf); //or "DHCP" for dynamic setting
-                setIpAddress(InetAddress.getByName(ip), 24, wifiConf);
-                setGateway(InetAddress.getByName(gateWay), wifiConf);
-                setDNS(InetAddress.getByName(dns), wifiConf);
-                manager.updateNetwork(wifiConf); //apply the setting
-                manager.saveConfiguration(); //Save it
-            }catch(Exception e){
                 e.printStackTrace();
             }
         }
     }
 
-    public static void setIpAssignment(String assign , WifiConfiguration wifiConf)
-            throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException{
-        setEnumField(wifiConf, assign, "ipAssignment");
-    }
-
-    public static void setIpAddress(InetAddress addr, int prefixLength, WifiConfiguration wifiConf)
-            throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException,
-            NoSuchMethodException, ClassNotFoundException, InstantiationException, InvocationTargetException{
-        Object linkProperties = getField(wifiConf, "linkProperties");
-        if(linkProperties == null)return;
-        Class laClass = Class.forName("android.net.LinkAddress");
-        Constructor laConstructor = laClass.getConstructor(new Class[]{InetAddress.class, int.class});
-        Object linkAddress = laConstructor.newInstance(addr, prefixLength);
-
-        ArrayList mLinkAddresses = (ArrayList)getDeclaredField(linkProperties, "mLinkAddresses");
-        mLinkAddresses.clear();
-        mLinkAddresses.add(linkAddress);
-    }
-
-    public static void setGateway(InetAddress gateway, WifiConfiguration wifiConf)
-            throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException,
-            ClassNotFoundException, NoSuchMethodException, InstantiationException, InvocationTargetException{
-        Object linkProperties = getField(wifiConf, "linkProperties");
-        if(linkProperties == null)return;
-        Class routeInfoClass = Class.forName("android.net.RouteInfo");
-        Constructor routeInfoConstructor = routeInfoClass.getConstructor(new Class[]{InetAddress.class});
-        Object routeInfo = routeInfoConstructor.newInstance(gateway);
-
-        ArrayList mRoutes = (ArrayList)getDeclaredField(linkProperties, "mRoutes");
-        mRoutes.clear();
-        mRoutes.add(routeInfo);
-    }
-
-    public static void setDNS(InetAddress dns, WifiConfiguration wifiConf)
-            throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException{
-        Object linkProperties = getField(wifiConf, "linkProperties");
-        if(linkProperties == null)return;
-
-        ArrayList<InetAddress> mDnses = (ArrayList<InetAddress>)getDeclaredField(linkProperties, "mDnses");
-        mDnses.clear(); //or add a new dns address , here I just want to replace DNS1
-        mDnses.add(dns);
-    }
-
-    public static Object getField(Object obj, String name)
-            throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException{
-        Field f = obj.getClass().getField(name);
-        Object out = f.get(obj);
-        return out;
-    }
-
-    public static Object getDeclaredField(Object obj, String name)
-            throws SecurityException, NoSuchFieldException,
-            IllegalArgumentException, IllegalAccessException {
-        Field f = obj.getClass().getDeclaredField(name);
-        f.setAccessible(true);
-        Object out = f.get(obj);
-        return out;
-    }
-
-    public static void setEnumField(Object obj, String value, String name)
-            throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException{
-        Field f = obj.getClass().getField(name);
-        f.set(obj, Enum.valueOf((Class<Enum>) f.getType(), value));
-    }
+    // [public_method]=============================[END]============================[public_method]
+    // [get/set]==================================[START]=================================[get/set]
+    // [get/set]===================================[END]==================================[get/set]
 
 }
